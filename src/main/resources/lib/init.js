8d42bc824b80e723cf683c4d1f76d53217f6a269
Alex Rodriguez <aro@enonic.com>
2018-02-22 12:58:55 +0100

    Rename app id to 'licensemanager' Rename repo to 'com.enonic.licensemanager'

--- src/main/resources/lib/init.js
+++ src/main/resources/lib/init.js
@@ -1,28 +1,8 @@
-var repoLib = require('/lib/xp/repo');
 var contextLib = require('/lib/xp/context');
-var nodeLib = require('/lib/xp/node');
-
-var REPO_NAME = 'license-repo';
-var APPLICATIONS_PATH = '/applications';
-
-var ROOT_PERMISSIONS = [
-    {
-        'principal': 'role:system.authenticated',
-        'allow': [
-            'READ',
-            'CREATE',
-            'MODIFY',
-            'DELETE',
-            'PUBLISH',
-            'READ_PERMISSIONS',
-            'WRITE_PERMISSIONS'
-        ],
-        'deny': []
-    }
-];
+var storeLib = require('/lib/store');
 
 exports.initialize = function () {
-    log.info('Initializing License repository...');
+    log.info('Initializing License Manager repository...');
 
     contextLib.run({
         user: {
@@ -30,55 +10,9 @@ exports.initialize = function () {
             userStore: 'system'
         },
         principals: ["role:system.admin"]
-    }, doInitialize);
+    }, storeLib.initialize);
 
 
-    log.info('License repository initialized.');
+    log.info('License Manager repository initialized.');
 };
 
-var doInitialize = function () {
-    var result = repoLib.get(REPO_NAME);
-
-    if (result) {
-        log.info('Repository found');
-    } else {
-        log.info('Repository not found');
-        createRepo();
-    }
-    createRootNodes();
-};
-
-var createRepo = function () {
-    log.info('Creating repository...');
-    var newRepo = repoLib.create({
-        id: REPO_NAME,
-        rootPermissions: ROOT_PERMISSIONS
-    });
-    log.info('Repository created.');
-};
-
-var createRootNodes = function () {
-    var repoConn = nodeLib.connect({
-        repoId: REPO_NAME,
-        branch: 'master'
-    });
-
-    var applicationsExist = nodeWithPathExists(repoConn, APPLICATIONS_PATH);
-    if (!applicationsExist) {
-        log.info('Creating node [' + APPLICATIONS_PATH + '] ...');
-        var appsNode = repoConn.create({
-            _name: APPLICATIONS_PATH.slice(1),
-            _parentPath: '/',
-            _permissions: ROOT_PERMISSIONS //TODO remove after XP issue 4801 is fixed
-        });
-    }
-};
-
-var nodeWithPathExists = function (repoConnection, path) {
-    var result = repoConnection.query({
-        start: 0,
-        count: 0,
-        query: "_path = '" + path + "'"
-    });
-    return result.total > 0;
-};
--- src/main/resources/lib/store.js
+++ src/main/resources/lib/store.js
@@ -1,8 +1,9 @@
 var nodeLib = require('/lib/xp/node');
+var repoLib = require('/lib/xp/repo');
 
-var REPO_NAME = 'license-repo';
+var REPO_NAME = 'com.enonic.licensemanager';
 var APPLICATIONS_PATH = '/applications';
-var NODE_PERMISSIONS = [
+var ROOT_PERMISSIONS = [
     {
         'principal': 'role:system.authenticated',
         'allow': [
@@ -77,6 +78,54 @@ exports.prettifyName = function (text) {
     return namePrettyfier.create(text);
 };
 
+exports.initialize= function () {
+    var result = repoLib.get(REPO_NAME);
+
+    if (result) {
+        log.info('Repository found');
+    } else {
+        log.info('Repository not found');
+        createRepo();
+    }
+    createRootNodes();
+};
+
+var createRepo = function () {
+    log.info('Creating repository...');
+    var newRepo = repoLib.create({
+        id: REPO_NAME,
+        rootPermissions: ROOT_PERMISSIONS
+    });
+    log.info('Repository created.');
+};
+
+var createRootNodes = function () {
+    var repoConn = nodeLib.connect({
+        repoId: REPO_NAME,
+        branch: 'master'
+    });
+
+    var applicationsExist = nodeWithPathExists(repoConn, APPLICATIONS_PATH);
+    if (!applicationsExist) {
+        log.info('Creating node [' + APPLICATIONS_PATH + '] ...');
+        var appsNode = repoConn.create({
+            _name: APPLICATIONS_PATH.slice(1),
+            _parentPath: '/',
+            _inheritsPermissions: true
+        });
+    }
+};
+
+var nodeWithPathExists = function (repoConnection, path) {
+    var result = repoConnection.query({
+        start: 0,
+        count: 0,
+        query: "_path = '" + path + "'"
+    });
+    return result.total > 0;
+};
+
+
 /**
  * Create a new application.
  *
@@ -101,7 +150,8 @@ exports.createApp = function (params) {
     var appNode = repoConn.create({
         _name: nodeName,
         _parentPath: APPLICATIONS_PATH,
-        _permissions: NODE_PERMISSIONS,
+        // _permissions: NODE_PERMISSIONS,
+        _inheritsPermissions: true,
         type: TYPE.APPLICATION,
         name: nodeName,
         displayName: params.displayName,
@@ -262,7 +312,8 @@ exports.createLicense = function (params) {
     var licenseNode = repoConn.create({
         _parentPath: app._path,
         _name: name,
-        _permissions: NODE_PERMISSIONS,
+        // _permissions: NODE_PERMISSIONS,
+        _inheritsPermissions: true,
         type: TYPE.LICENSE,
         issuedBy: params.issuedBy,
         issuedTo: params.issuedTo,
