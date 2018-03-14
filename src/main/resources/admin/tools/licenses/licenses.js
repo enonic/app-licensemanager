3da3f5d40a2323e1f1ec727b264110635dd7822c
Alex Rodriguez <aro@enonic.com>
2018-03-14 07:18:34 +0100

    Fixed issue with asset URLs

--- src/main/resources/admin/tools/licenses/licenses.js
+++ src/main/resources/admin/tools/licenses/licenses.js
@@ -1,30 +1,14 @@
 var mustacheLib = require('/lib/xp/mustache');
 var portalLib = require('/lib/xp/portal');
-var authLib = require('/lib/xp/auth');
 
 var view = resolve('licenses.html');
 
 exports.get = function () {
-    var user = authLib.getUser();
-    if (!user) {
-        return {
-            status: 401
-        }
-    }
-
-    var appBaseUrl = '/app/' + app.name;
-    var t = new Date();
-    var t2 = new Date();
-    t2.setFullYear(t2.getFullYear() + 1);
-
     var svcUrl = portalLib.serviceUrl({service: 'Z'}).slice(0, -1);
 
     var params = {
-        assetsUri: appBaseUrl,
-        svcUrl: svcUrl,
-        currentUser: user.key || '',
-        issueTime: t.toISOString(),
-        expiryTime: t2.toISOString()
+        assetsUri: portalLib.assetUrl({path: ""}),
+        svcUrl: svcUrl
     };
 
     var body = mustacheLib.render(view, params);
