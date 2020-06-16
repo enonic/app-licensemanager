d7e462305e6961bf06f545383326c58bf0a8d241
Christian Westgaard <cwe@enonic.com>
2020-06-16 14:02:18 +0200

    Adding XP Menu #14 (lib-admin-ui not required)

--- src/main/resources/admin/tools/licenses/licenses.js
+++ src/main/resources/admin/tools/licenses/licenses.js
@@ -1,20 +1,33 @@
+var adminLib = require('/lib/xp/admin');
 var mustacheLib = require('/lib/mustache');
 var portalLib = require('/lib/xp/portal');
 
-var view = resolve('licenses.html');
+
+var assetUrl = portalLib.assetUrl;
+var getBaseUri = adminLib.getBaseUri;
+var getLauncherPath = adminLib.getLauncherPath;
+var getLauncherUrl = adminLib.getLauncherUrl;
+var render = mustacheLib.render;
+var serviceUrl = portalLib.serviceUrl;
+
+
+var VIEW = resolve('licenses.html');
+
 
 exports.get = function () {
-    var svcUrl = portalLib.serviceUrl({service: 'Z'}).slice(0, -1);
+    var svcUrl = serviceUrl({service: 'Z'}).slice(0, -1);
 
     var params = {
-        assetsUri: portalLib.assetUrl({path: ""}),
+        adminUrl: getBaseUri(),
+        appId: app.name,
+        assetsUri: assetUrl({path: ''}),
+        launcherPath: getLauncherPath(),
+        launcherUrl: getLauncherUrl(),
         svcUrl: svcUrl
     };
 
-    var body = mustacheLib.render(view, params);
-
     return {
         contentType: 'text/html',
-        body: body
+        body: render(VIEW, params)
     }
 };
